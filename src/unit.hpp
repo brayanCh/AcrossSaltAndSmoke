c53b8bcda7091175823ae7a5ef71681bf86377e0
brayanch <brayanchLaboral@outlook.es>
2022-06-05 14:45:30 -0500

    beggining with c++

---
+++ src/unit.hpp
@@ -0,0 +1,34 @@
+// A `player.gdns` file has already been created for you. Attach it to the  node.
+
+// Create two files `player.cpp` and `player.hpp` next to `entry.cpp` in `src`.
+// This code goes in `player.hpp`. We also define the methods we'll be using here.
+#ifndef UNIT_H
+#define UNIT_H
+
+#include <AnimatedSprite.hpp>
+#include <Area2D.hpp>
+#include <CollisionShape2D.hpp>
+#include <Godot.hpp>
+#include <Input.hpp>
+
+class Unit : public godot::Area2D {
+    GODOT_CLASS(Unit, godot::Area2D)
+
+    godot::AnimatedSprite *_animated_sprite;
+    godot::CollisionShape2D *_collision_shape;
+    godot::Input *_input;
+    godot::Vector2 _screen_size; // Size of the game window.
+
+public:
+    real_t speed = 400; // How fast the player will move (pixels/sec).
+
+    void _init() {}
+    void _ready();
+    void _process(const double p_delta);
+    void start(const godot::Vector2 p_position);
+    void _on_Unit_body_entered(godot::Node2D *_body);
+
+    static void _register_methods();
+};
+
+#endif // UNIT_H
